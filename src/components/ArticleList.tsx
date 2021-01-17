f0c698c857b37435d74c3a4769e1d4a220ac3ceb
Manish Kumar <manish.k@taskworld.com>
2021-01-17 23:48:59 +0700

    code cleanup

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -3,34 +3,6 @@ import React from "react";
 import FallBack_THUMBNAIL from "../nytimes-logo.png";
 
 export default function ArticleList({ articles }: { articles: any[] }) {
-  //   const [articles, setArticles] = useState<any[] | undefined>(undefined);
-  //   useEffect(() => {
-  //     fetch(
-  //       "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=TnydJyKOpuvAhSpWPgWWLXUHqS0M6zqO"
-  //     ).then(async (res) => {
-  //       const json = await res.json();
-  //       setArticles(json.response.docs);
-  //     });
-  //   }, []);
-
-  //   if (articles === undefined) {
-  //     return <div>Loading...</div>;
-  //   }
-
-  //   const listData = articles.map((article) => ({
-  //     id: article._id,
-  //     title: article.headline.main,
-  //     thumbnail: article.multimedia.find(
-  //       (media: any) => media.type === "image" && media.subType === "thumbLarge"
-  //     )?.url,
-  //     description: article.abstract,
-  //     content: article.lead_paragraph,
-  //   }));
-
-  //   for (let i = 0; i < articles.length; i++) {
-  //     listData.push();
-  //   }
-
   return (
     <List
       itemLayout="vertical"
